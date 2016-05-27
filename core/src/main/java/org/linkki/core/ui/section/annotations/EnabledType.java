7f8512247e45b47b7cf3c05090791b94ae5db636
Christian Asselmeyer <christian.asselmeyer@faktorzehn.at>
2016-05-27 12:32:03 +0200

    Added JavaDoc for EnabledType enum.
    Change-Id: Ic3104fa3b8b3445cf0de71bd9fe36b8df908979a

--- core/src/main/java/org/linkki/core/ui/section/annotations/EnabledType.java
+++ core/src/main/java/org/linkki/core/ui/section/annotations/EnabledType.java
@@ -1,9 +1,17 @@
 package org.linkki.core.ui.section.annotations;
 
+/** The type how the enabled state of an UI component is bound. */
 public enum EnabledType {
 
+    /** The UI component is always enabled. */
     ENABLED,
+
+    /** The UI component is always disabled. */
     DISABLED,
+
+    /**
+     * The enabled state is read from the PMO by invoking a method named is[PropertyName]Enabled().
+     */
     DYNAMIC;
 
 }
