c378ef4aa739ff96d139122921d0eb963c2b63f6
hjiang <hengrui.jiang@faktorzehn.de>
2017-05-10 20:00:23 +0200

    LIN-328 :: change css prefix to linkki
    Change-Id: Ib23f71eda7643abed4d491cc29b1fde2690302e5

--- core/src/main/java/org/linkki/core/ui/application/ApplicationStyles.java
+++ core/src/main/java/org/linkki/core/ui/application/ApplicationStyles.java
@@ -10,66 +10,66 @@ public class ApplicationStyles {
     /**
      * Style for the header section of the application.
      */
-    public static final String APPLICATION_HEADER = "f10-application-header";
+    public static final String APPLICATION_HEADER = "linkki-application-header";
 
     /**
      * Style for the menu section of the application.
      */
-    public static final String MENU_WRAPPER = "f10-menu-wrapper";
+    public static final String MENU_WRAPPER = "linkki-menu-wrapper";
 
-    public static final String HORIZONTAL_SPACER = "f10-horizontal-spacer";
+    public static final String HORIZONTAL_SPACER = "linkki-horizontal-spacer";
 
-    public static final String FIXED_HORIZONTAL_SPACER = "f10-fixed-horizontal-spacer";
+    public static final String FIXED_HORIZONTAL_SPACER = "linkki-fixed-horizontal-spacer";
 
     /**
      * Style for the top-level items of the application's main menu in the header.
      */
-    public static final String APPLICATION_MENU = "f10-application-menu";
+    public static final String APPLICATION_MENU = "linkki-application-menu";
 
     /**
      * Style for the caption/header of a modal dialog.
      */
-    public static final String DIALOG_CAPTION = "f10-dialog-caption";
+    public static final String DIALOG_CAPTION = "linkki-dialog-caption";
 
     /**
      * Style for the body of a modal dialog.
      */
-    public static final String DIALOG_CONTENT = "f10-dialog-content";
+    public static final String DIALOG_CONTENT = "linkki-dialog-content";
 
     /**
      * Style for the caption of a section.
      * 
      * @see AbstractSection
      */
-    public static final String SECTION_CAPTION = "f10-section-caption";
+    public static final String SECTION_CAPTION = "linkki-section-caption";
 
     /**
      * Style for the caption of a section.
      * 
      * @see AbstractSection
      */
-    public static final String SECTION_CAPTION_TEXT = "f10-section-caption-text";
+    public static final String SECTION_CAPTION_TEXT = "linkki-section-caption-text";
 
     /**
      * Style for the line separating the section's header/caption from the body/content.
      * 
      * @see AbstractSection
      */
-    public static final String SECTION_CAPTION_LINE = "f10-section-caption-line";
+    public static final String SECTION_CAPTION_LINE = "linkki-section-caption-line";
     /**
      * Style for the line separating the section's header/caption from the body/content.
      * 
      * @see AbstractSection
      */
-    public static final String LOGIN_PANEL_CAPTION = "f10-login-panel-caption";
+    public static final String LOGIN_PANEL_CAPTION = "linkki-login-panel-caption";
 
-    public static final String LOGIN_PANEL = "f10-login-panel";
+    public static final String LOGIN_PANEL = "linkki-login-panel";
 
     public static final String BORDERLESS = "borderless";
 
-    public static final String TABLE = "f10-table";
+    public static final String TABLE = "linkki-table";
 
-    public static final String TABLE_CELL = "f10-table-cell";
+    public static final String TABLE_CELL = "linkki-table-cell";
 
     public static final String SPACING_HORIZONTAL_SECTION = "horizontal-section-spacing";
 
