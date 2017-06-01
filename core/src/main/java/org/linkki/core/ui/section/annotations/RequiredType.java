d5dd53f832ac859c5b6eabb9df69b71dfab6df45
Daniel Schwering <daniel.schwering@faktorzehn.de>
2017-06-01 09:17:06 +0200

    LIN-387/LIN-398 :: Binding Properties
    Change-Id: I60837425eeb6067e68b25c18ac68d4acdbea3fdf

--- core/src/main/java/org/linkki/core/ui/section/annotations/RequiredType.java
+++ core/src/main/java/org/linkki/core/ui/section/annotations/RequiredType.java
@@ -1,10 +1,24 @@
 package org.linkki.core.ui.section.annotations;
 
+/**
+ * Defines how the required state (whether a value must be entered/selected) of an UI component is
+ * bound.
+ */
 public enum RequiredType {
 
+    /** The UI component is always required. */
     REQUIRED,
+
+    /** The UI component is required if the component is {@link EnabledType enabled}. */
     REQUIRED_IF_ENABLED,
+
+    /** The UI component is optional. */
     NOT_REQUIRED,
+
+    /**
+     * The required state is read from the PMO by invoking a method named
+     * is[PropertyName]Required().
+     */
     DYNAMIC,
 
 }
