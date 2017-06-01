6af814e3310acb297bd73332a9758d6856f95ed0
Daniel Schwering <daniel.schwering@faktorzehn.de>
2017-06-01 14:57:45 +0200

    LIN-387/LIN-399 :: vertical binding
    Change-Id: I24cf842c3845dd8dbd1228aadbf4cf5cb520a7fb

---
+++ doc/src/main/java/org/linkki/doc/ReadOnlyBehavior.java
@@ -0,0 +1,19 @@
+package org.linkki.doc;
+
+import org.linkki.core.binding.aspect.PropertyBehavior;
+
+//tag::class[]
+class ReadOnlyBehavior implements PropertyBehavior {
+
+    private boolean editable;
+
+    public ReadOnlyBehavior(boolean editable) {
+       this.editable = editable;
+    }
+    
+    @Override
+    public boolean isWritable(Object boundObject, String property) {
+        return editable;
+    }
+}
+//end::class[]
