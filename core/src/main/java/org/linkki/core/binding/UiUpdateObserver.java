a643e851a16b4d9cd6c7896eadd677b17ce0531a
Daniel Schwering <daniel.schwering@faktorzehn.de>
2017-07-13 08:40:48 +0200

    FIPM-792/FIPM-1637 :: add andThen to UiUpdateObserver to allow chaining of observers
    Change-Id: I045ac909ceceb560c47464ec67eb2a1ebca9a7a2

--- core/src/main/java/org/linkki/core/binding/UiUpdateObserver.java
+++ core/src/main/java/org/linkki/core/binding/UiUpdateObserver.java
@@ -6,12 +6,15 @@
 
 package org.linkki.core.binding;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * An observer that can be attached to a {@link BindingManager} and is notified if an update UI
  * event is triggered by a managed {@link BindingContext}. Updates its own UI upon notification.
  * 
  * @see BindingManager#addUiUpdateObserver(UiUpdateObserver)
  */
+@FunctionalInterface
 public interface UiUpdateObserver {
 
     /**
@@ -19,4 +22,24 @@ public interface UiUpdateObserver {
      * {@link BindingContext}s).
      */
     void uiUpdated();
+
+    /**
+     * Returns a composed {@code UiUpdateObserver} that performs, in sequence, this observer's
+     * {@link #uiUpdated()} method followed by the {@code after} observer's {@link #uiUpdated()}
+     * method. If performing either update throws an exception, it is relayed to the caller of the
+     * composed observer. If performing this update throws an exception, the {@code after} observer
+     * will not be notified.
+     *
+     * @param after the observer to notify after this observer
+     * @return a composed {@code UiUpdateObserver} that notifies in sequence this observer followed
+     *         by the {@code after} observer
+     * @throws NullPointerException if {@code after} is null
+     */
+    default UiUpdateObserver andThen(UiUpdateObserver after) {
+        requireNonNull(after, "after must not be null");
+        return () -> {
+            uiUpdated();
+            after.uiUpdated();
+        };
+    }
 }
