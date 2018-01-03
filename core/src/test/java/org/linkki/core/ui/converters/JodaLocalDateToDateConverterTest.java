6a6101a7e1f38fa525e5bb571fc1778c3e3d3532
dirmeier <cornelius.dirmeier@faktorzehn.de>
2018-01-03 10:45:49 +0100

    LIN-693/LIN-695 :: deploy doc with wagon
    Change-Id: Id8f959a40495c26a2330206407430802ba4a9ef1

--- core/src/test/java/org/linkki/core/ui/converters/JodaLocalDateToDateConverterTest.java
+++ core/src/test/java/org/linkki/core/ui/converters/JodaLocalDateToDateConverterTest.java
@@ -45,19 +45,44 @@ public class JodaLocalDateToDateConverterTest {
 
     @Parameterized.Parameters
     public static Object[][] parameters() {
+        final String shortYearPattern = "yy.MM.dd";
+        final String longYearPattern = "yyyy.MM.dd";
+        final String germanLongPattern = "dd.MM.yyyy";
+        final int deltaDays = 1000;
+        final int nearly19Years = 19 * 365;
+        final int nearly80Years = 80 * 365;
 
         return new Object[][] {
                 /* [0] */ { null, "yy.MM.dd", null },
-                /* [1] */ { "2016.12.29", "yyyy.MM.dd", new LocalDate(2016, 12, 29) },
-                /* [2] */ { "16.12.29", "yy.MM.dd", new LocalDate(2016, 12, 29) },
-                /* [3] */ { "0016.12.29", "yyyy.MM.dd", new LocalDate(2016, 12, 29) },
-                /* [4] */ { "0036.12.29", "yyyy.MM.dd", new LocalDate(2036, 12, 29) },
-                /* [5] */ { "36.12.29", "yy.MM.dd", new LocalDate(2036, 12, 29) },
-                /* [6] */ { "0037.12.29", "yyyy.MM.dd", new LocalDate(1937, 12, 29) },
-                /* [7] */ { "84.12.29", "yy.MM.dd", new LocalDate(1984, 12, 29) }
+                /* [1] */ { "2016.12.29", longYearPattern, new LocalDate(2016, 12, 29) },
+                /* [2] */ { "01.01.1969", germanLongPattern, new LocalDate(1969, 01, 01) },
+                /* [3] */ { "01.01.1900", germanLongPattern, new LocalDate(1900, 01, 01) },
+                /* [4] */ { "01.01.1899", germanLongPattern, new LocalDate(1899, 01, 01) },
+                /* [5] */ { "02.04.1893", germanLongPattern, new LocalDate(1893, 04, 02) },
+                /* [6] */ { "01.04.1893", germanLongPattern, new LocalDate(1893, 04, 01) },
+                /* [7] */ { "01.01.1800", germanLongPattern, new LocalDate(1800, 01, 01) },
+                /* [8] */ { "01.01.1800", germanLongPattern, new LocalDate(1800, 01, 01) },
+                /* [9] */ { "28.02.1700", germanLongPattern, new LocalDate(1700, 02, 28) },
+                /* [10] */ { "29.02.1600", germanLongPattern, new LocalDate(1600, 02, 29) },
+                /* [11] */ { "04.02.1598", germanLongPattern, new LocalDate(1598, 02, 04) },
+                /* [12] */ { "04.02.1598", germanLongPattern, new LocalDate(1598, 02, 04) },
+                // @formatter:off
+                /* [13] */ { getDateFromNow(deltaDays).toString(shortYearPattern), shortYearPattern,getDateFromNow(deltaDays) },
+                /* [14] */ { getDateFromNow(-deltaDays).toString(shortYearPattern), shortYearPattern,getDateFromNow(-deltaDays) },
+                /* [15] */ { "00" + getDateFromNow(deltaDays).toString(shortYearPattern), longYearPattern,getDateFromNow(deltaDays) },
+                /* [16] */ { "00" + getDateFromNow(-deltaDays).toString(shortYearPattern), longYearPattern,getDateFromNow(-deltaDays) },
+                /* [17] */ { "00" + getDateFromNow(-nearly80Years).toString(shortYearPattern), longYearPattern,getDateFromNow(-nearly80Years) },
+                /* [18] */ { "00" + getDateFromNow(nearly19Years).toString(shortYearPattern), longYearPattern,getDateFromNow(nearly19Years) },
+                /* [19] */ { getDateFromNow(0).toString(shortYearPattern), shortYearPattern, getDateFromNow(0) }, // today
+                /* [20] */ { "00" + getDateFromNow(0).toString(shortYearPattern), longYearPattern, getDateFromNow(0) } // today
+                // @formatter:on
         };
     }
 
+    private static final LocalDate getDateFromNow(int days) {
+        return LocalDate.now().plusDays(days);
+
+    }
 
     @Test
     public void testConvertToModel() throws Exception {
