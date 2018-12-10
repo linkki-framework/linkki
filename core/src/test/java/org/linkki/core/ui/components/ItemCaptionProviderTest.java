fdd73371f07c55e2ab08727e87785023b1c8debd
Cornelius Dirmeier <cornelius.dirmeier@faktorzehn.de>
2018-12-10 09:47:30 +0100

    LIN-1011/LIN-1164 :: switch AG - GmbH
    Change-Id: I13427644ab953b3ef0eb2d699fcdd10384e8be04

--- core/src/test/java/org/linkki/core/ui/components/ItemCaptionProviderTest.java
+++ core/src/test/java/org/linkki/core/ui/components/ItemCaptionProviderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Faktor Zehn AG.
+ * Copyright Faktor Zehn GmbH.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
