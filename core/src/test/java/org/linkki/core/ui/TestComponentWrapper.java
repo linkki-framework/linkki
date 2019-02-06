7fcc14241e8799022b0125acb1121869d906a19f
Ancy Kuruvilla <ancy.kuruvilla@faktorzehn.de>
2019-02-06 11:14:31 +0100

    LIN-1062/LIN-1254 :: new tests for aspect definition classes
    Change-Id: If1c8169cd7797a92d31277e3b84f6fbe92f14d81

--- core/src/test/java/org/linkki/core/ui/TestComponentWrapper.java
+++ core/src/test/java/org/linkki/core/ui/TestComponentWrapper.java
@@ -1,17 +1,15 @@
 /*
  * Copyright Faktor Zehn GmbH.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and limitations under the
+ * License.
  */
 
 package org.linkki.core.ui;
@@ -20,7 +18,7 @@ import org.linkki.core.message.MessageList;
 import org.linkki.core.ui.components.ComponentWrapper;
 import org.linkki.core.ui.components.WrapperType;
 
-class TestComponentWrapper implements ComponentWrapper {
+public class TestComponentWrapper implements ComponentWrapper {
 
     private static final long serialVersionUID = 1L;
 
---
+++ core/src/test/java/org/linkki/core/ui/section/annotations/aspect/EnabledAspectDefinitionTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright Faktor Zehn GmbH.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and limitations under the
+ * License.
+ */
+
+package org.linkki.core.ui.section.annotations.aspect;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.function.Consumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.linkki.core.binding.aspect.Aspect;
+import org.linkki.core.ui.TestComponentWrapper;
+import org.linkki.core.ui.TestUiComponent;
+import org.linkki.core.ui.section.annotations.EnabledType;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EnabledAspectDefinitionTest {
+
+    private TestComponentWrapper componentWrapper = new TestComponentWrapper(new TestUiComponent());
+
+    @Test
+    public void testCreateAspect_enabled() {
+        EnabledAspectDefinition aspectDefinition = new EnabledAspectDefinition(EnabledType.ENABLED);
+
+        Aspect<Boolean> createdAspect = aspectDefinition.createAspect();
+
+        assertThat(createdAspect.getName(), is(EnabledAspectDefinition.NAME));
+        assertThat(createdAspect.getValue(), is(true));
+    }
+
+    @Test
+    public void testCreateAspect_dynamic() {
+        EnabledAspectDefinition aspectDefinition = new EnabledAspectDefinition(EnabledType.DYNAMIC);
+
+        Aspect<Boolean> createdAspect = aspectDefinition.createAspect();
+
+        assertThat(createdAspect.getName(), is(EnabledAspectDefinition.NAME));
+        assertThat(createdAspect.isValuePresent(), is(false));
+    }
+
+    @Test
+    public void testCreateComponentValueSetterComponentWrapper() {
+        EnabledAspectDefinition aspectDefinition = new EnabledAspectDefinition(
+                EnabledType.ENABLED);
+        Consumer<Boolean> setter = aspectDefinition.createComponentValueSetter(componentWrapper);
+
+        assertThat(componentWrapper.getComponent().isEnabled(), is(false));
+
+        setter.accept(true);
+
+        assertThat(componentWrapper.getComponent().isEnabled(), is(true));
+    }
+
+}
