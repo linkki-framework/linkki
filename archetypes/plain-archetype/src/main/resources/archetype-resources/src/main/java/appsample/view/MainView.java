3e0cf88dd6b0b71a9f0ae3f143cbae38be52616d
Ancy Kuruvilla <ancy.kuruvilla@faktorzehn.de>
2019-02-18 17:06:44 +0100

    LIN-1277/LIN-1278 :: intorduce linkki archetypes
    Change-Id: Ia9924c82683fa714dc8874fc5332510f626776c9

---
+++ archetypes/plain-archetype/src/main/resources/archetype-resources/src/main/java/appsample/view/MainView.java
@@ -0,0 +1,32 @@
+package \${package}.appsample.view;
+
+import org.linkki.framework.ui.component.Headline;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.VerticalLayout;
+
+import \${package}.appsample.page.${ApplicationName}Page;
+
+public class MainView extends VerticalLayout implements View {
+
+    private static final long serialVersionUID = 1L;
+
+    public static final String NAME = "";
+
+    public MainView() {
+        addComponent(new Headline("linkki Example Web Application"));
+        \${ApplicationName}Page page = new ${ApplicationName}Page();
+        page.createContent();
+        addComponent(page);
+
+        setSizeFull();
+        setExpandRatio(page, 1f);
+    }
+
+    @Override
+    public void enter(ViewChangeEvent event) {
+        // TODO Auto-generated method stub
+    }
+
+}
---
+++ archetypes/spring-archetype/src/main/resources/archetype-resources/src/main/java/appsample/view/MainView.java
@@ -0,0 +1,38 @@
+package ${package}.appsample.view;
+
+import org.linkki.framework.ui.component.Headline;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.VerticalLayout;
+
+import ${package}.appsample.page.${ApplicationName}Page;
+
+
+@SpringView(name = MainView.NAME)
+public class MainView extends VerticalLayout implements View {
+
+    private static final long serialVersionUID = 1L;
+
+    public static final String NAME = "";
+
+    @Autowired
+    public MainView() {
+        addComponent(new Headline("linkki Example Web Application"));
+        ${ApplicationName}Page page = new ${ApplicationName}Page();
+        page.createContent();
+        addComponent(page);
+
+        setSizeFull();
+        setExpandRatio(page, 1f);
+    }
+
+    @Override
+    public void enter(ViewChangeEvent event) {
+        // TODO Auto-generated method stub
+    }
+
+
+}
