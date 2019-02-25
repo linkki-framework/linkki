8f751eb6a406bf6c034bf60bac95fe427cc3d840
Cornelius Dirmeier <cornelius.dirmeier@faktorzehn.de>
2019-02-25 15:56:16 +0100

    LIN-1238/LIN-1323 :: fix comboBox filter with @Bind
    Change-Id: I89cc371d451b7e0c62839f76186d529a838b30f9

--- vaadin8/core/src/main/java/org/linkki/core/ui/section/annotations/aspect/HasItemsAvailableValuesAspectDefinition.java
+++ vaadin8/core/src/main/java/org/linkki/core/ui/section/annotations/aspect/HasItemsAvailableValuesAspectDefinition.java
@@ -43,6 +43,8 @@ public class HasItemsAvailableValuesAspectDefinition extends AvailableValuesAspe
     private static void setDataProvider(HasItems<?> component, ListDataProvider<Object> listDataProvider) {
         if (component instanceof HasDataProvider) {
             ((HasDataProvider<Object>)component).setDataProvider(listDataProvider);
+        } else if (component instanceof ComboBox) {
+            ((ComboBox<Object>)component).setDataProvider(listDataProvider);
         } else if (component instanceof HasFilterableDataProvider) {
             ((HasFilterableDataProvider<Object, SerializablePredicate<Object>>)component)
                     .setDataProvider(listDataProvider);
