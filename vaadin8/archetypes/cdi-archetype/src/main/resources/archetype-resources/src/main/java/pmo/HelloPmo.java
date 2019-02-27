c6e4871a377211230ac1be72932b7cab0a9a3e2f
Daniel Schwering <daniel.schwering@faktorzehn.de>
2019-02-27 12:19:57 +0100

    LIN-1277/LIN-1303 :: Move archetypes to vaadin8 module
    Change-Id: I900bb7adc78b602b2dcea17b508b09d95c17d3a7

---
+++ vaadin8/archetypes/cdi-archetype/src/main/resources/archetype-resources/src/main/java/pmo/HelloPmo.java
@@ -0,0 +1,55 @@
+package \${package}.pmo;
+
+import org.apache.commons.lang3.StringUtils;
+import org.linkki.core.ui.section.annotations.EnabledType;
+import org.linkki.core.ui.section.annotations.UIButton;
+import org.linkki.core.ui.section.annotations.UILabel;
+import org.linkki.core.ui.section.annotations.UISection;
+import org.linkki.core.ui.section.annotations.UITextField;
+import org.linkki.core.ui.section.annotations.VisibleType;
+import org.linkki.util.handler.Handler;
+
+import com.vaadin.icons.VaadinIcons;
+
+@UISection
+public class HelloPmo {
+
+    private String name;
+    private Handler handler;
+
+    public HelloPmo(Handler handler) {
+        this.handler = handler;
+    }
+
+    @UITextField(position = 10, label = "Please type your Name and press Enter")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @UILabel(position = 20)
+    public String getHello() {
+        if (name != null) {
+            return "Hello " + name + "! This is a linkki Example Web Application.";
+        } else {
+            return "";
+        }
+    }
+
+    @UIButton(position = 30, showIcon = true, icon = VaadinIcons.INFO, enabled = EnabledType.DYNAMIC, visible = VisibleType.DYNAMIC)
+    public void button() {
+        handler.apply();
+    }
+
+    public boolean isButtonEnabled() {
+        return StringUtils.isNoneBlank(name);
+    }
+
+    public boolean isButtonVisible() {
+        return StringUtils.isNoneBlank(name);
+    }
+
+}
