6dfebd5ecc981a220c58950e1a58accdd91930fe
Cornelius Dirmeier <cornelius.dirmeier@faktorzehn.de>
2019-05-21 18:13:39 +0200

    FCOM-129/FCOM-130 :: minor doc fixes in Objects.java
    Change-Id: I91f81617eeb1ec9468174ef5d7c39de570bc701b

--- utils/src/main/java/org/linkki/util/Objects.java
+++ utils/src/main/java/org/linkki/util/Objects.java
@@ -14,34 +14,36 @@
 
 package org.linkki.util;
 
+import java.util.function.Supplier;
+
 import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
-import edu.umd.cs.findbugs.annotations.Nullable;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
-public class Objects {
+/**
+ * Provides {@code static} utility methods that delegate to {@link java.util.Objects} and are
+ * additionally annotated with SpotBugs {@code null} annotations.
+ */
+public final class Objects {
 
     private Objects() {
-        // do not instantiate
+        // Utility class
     }
 
     /**
      * Checks that the specified object reference is not {@code null} and throws a customized
      * {@link NullPointerException} if it is. This method is designed primarily for doing parameter
      * validation in methods and constructors with multiple parameters, as demonstrated below:
-     * <blockquote>
      * 
-     * <pre>
+     * <code>
      * public Foo(Bar bar, Baz baz) {
      *     this.bar = Objects.requireNonNull(bar, "bar must not be null");
      *     this.baz = Objects.requireNonNull(baz, "baz must not be null");
      * }
-     * </pre>
-     * 
-     * </blockquote>
+     * </code>
      *
-     * Wrapper for {@link java.util.Objects#requireNonNull(Object)} that is annotated with spotbugs
-     * annotations to correctly handle the null case.
+     * Wrapper for {@link java.util.Objects#requireNonNull(Object, String)} that is annotated with
+     * SpotBugs annotations to correctly handle the null case.
      * <p>
      * Will be removed when issue https://github.com/spotbugs/spotbugs/issues/456 is fixed
      *
@@ -52,10 +54,38 @@ public class Objects {
      * @return {@code obj} if not {@code null}
      * @throws NullPointerException if {@code obj} is {@code null}
      */
-    @SuppressFBWarnings(value = "NP_PARAMETER_MUST_BE_NONNULL_BUT_MARKED_AS_NULLABLE", justification = "thats the trick of this methos")
+    @SuppressFBWarnings(value = "NP_PARAMETER_MUST_BE_NONNULL_BUT_MARKED_AS_NULLABLE", justification = "that's the trick of this method")
     @NonNull
-    public static <T> T requireNonNull(@Nullable @CheckForNull T obj, String message) {
+    public static <T> T requireNonNull(@CheckForNull T obj, @NonNull String message) {
         return java.util.Objects.requireNonNull(obj, message);
     }
 
+    /**
+     * Checks that the specified object reference is not {@code null} and throws a customized
+     * {@link NullPointerException} if it is.
+     *
+     * <p>
+     * Unlike the method {@link #requireNonNull(Object, String)}, this method allows creation of the
+     * message to be deferred until after the null check is made. While this may confer a performance
+     * advantage in the non-null case, when deciding to call this method care should be taken that the
+     * costs of creating the message supplier are less than the cost of just creating the string message
+     * directly.
+     *
+     * Wrapper for {@link java.util.Objects#requireNonNull(Object, Supplier)} that is annotated with
+     * SpotBugs annotations to correctly handle the null case.
+     * <p>
+     * Will be removed when issue https://github.com/spotbugs/spotbugs/issues/456 is fixed
+     *
+     * @param obj the object reference to check for nullity
+     * @param messageSupplier detail message supplier to be used in the event that a {@code
+     *                NullPointerException} is thrown
+     * @param <T> the type of the reference
+     * @return {@code obj} if not {@code null}
+     * @throws NullPointerException if {@code obj} is {@code null}
+     */
+    @SuppressFBWarnings(value = "NP_PARAMETER_MUST_BE_NONNULL_BUT_MARKED_AS_NULLABLE", justification = "that's the trick of this method")
+    @NonNull
+    public static <T> T requireNonNull(@CheckForNull T obj, @NonNull Supplier<String> messageSupplier) {
+        return java.util.Objects.requireNonNull(obj, messageSupplier);
+    }
 }
