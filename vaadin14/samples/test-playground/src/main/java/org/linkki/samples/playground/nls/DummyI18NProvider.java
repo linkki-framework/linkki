f3c1972f7af11ff669c8d26821ad957636cf4e22
Stefan Brenner <stefan.brenner@faktorzehn.at>
2021-01-12 14:06:18 +0100

    LIN-2132/LIN-2134 :: Run samples with spring boot
    Change-Id: Ie73137087e0f9979e55127286ec92e97121f00fe

--- vaadin14/samples/test-playground/src/main/java/org/linkki/samples/playground/nls/DummyI18NProvider.java
+++ vaadin14/samples/test-playground/src/main/java/org/linkki/samples/playground/nls/DummyI18NProvider.java
@@ -18,6 +18,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
+import org.springframework.stereotype.Component;
+
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.i18n.I18NProvider;
 
@@ -26,6 +28,7 @@ import com.vaadin.flow.i18n.I18NProvider;
  * {@link UI#getLocale()} always returns {@link Locale#getDefault()} if no {@link I18NProvider} is
  * registered.
  */
+@Component
 public class DummyI18NProvider implements I18NProvider {
 
     private static final long serialVersionUID = 1L;
