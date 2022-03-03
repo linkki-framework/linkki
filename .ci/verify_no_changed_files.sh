24faba38cb28cd91c2498f879207862cd2d13240
Lukas Gönner <lukas.goenner@faktorzehn.de>
2022-03-03 15:50:13 +0100

    FCOM-872/FCOM-874 :: Use verifyNoChangedFiles from pipeline library
    Change-Id: Ief577b989f24b7722d7363f5bec9737abf14ba91

--- .ci/verify_no_changed_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# verify zero changes in generated code
-# exit with 1 if there are any changes
-# exit with 0 if there are no other changes
-
-echo "Verify that no changes have resulted from the build."
-echo "If there are any changes due to vaadin-maven-plugin, run your local build in production mode."
-
-git status --porcelain | grep -vs "any changes" && echo -e \'\\e[31mTHERE ARE UNCOMITTED CHANGES\\e[0m\' && exit 1 || exit 0
