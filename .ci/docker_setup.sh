9338b64fb7d09bb45e3ad0364b67196b72d1c986
Hengrui Jiang <hengrui.jiang@faktorzehn.de>
2023-06-27 10:12:40 +0200

    FCOM-1194/FCOM-1646 :: Use base image from harbor
    Change-Id: I614a12bded79353b7bae7e0914527975f4891efc

--- .ci/docker_setup.sh
+++ .ci/docker_setup.sh
@@ -14,17 +14,19 @@ if [ -n "$(docker container ls --filter="name=^$CONTAINER_NAME$" -a -q)" ]; then
 fi
 
 docker create \
-        --cpus=2 --memory=4g \
+        --cpus=2 --memory=1g \
         --name $CONTAINER_NAME \
         --network $NETWORK_NAME \
         --label "url=$CONTAINER_NAME" \
         --label "entry-path=linkki-sample-test-playground-vaadin23" \
         --label "retention=${CONTAINER_RETENTION:-discard}" \
-        f10/spring:11
+        harbor.faktorzehn.de/suite-base/spring:22.12
 
-# Copy war to container
+# Copy WAR to container
 WAR_FILE="vaadin23/samples/test-playground/target/linkki-sample-test-playground-vaadin23.war"
-docker cp $WAR_FILE $CONTAINER_NAME:/opt/spring/application.war
+# Container path must use application.jar in order to work with the harbor image.
+# This still works even if a WAR is copied.
+docker cp $WAR_FILE $CONTAINER_NAME:/opt/spring/application.jar
 
 # Start the container
 docker start $CONTAINER_NAME
