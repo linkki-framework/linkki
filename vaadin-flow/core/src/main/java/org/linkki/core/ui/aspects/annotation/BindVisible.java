d340262d1522d7b4268e3e3ce2761856dd1f4057
Sebastian Zimmerer <sebastian.zimmerer@faktorzehn.de>
2023-12-19 14:15:34 +0100

    LIN-3552/LIN-3665 :: Add documentation for initial update of even invisible PMOs
    Change-Id: Ia2f2f8bc70aca688a30a8e76d6113cdf41cca9b5

--- vaadin-flow/core/src/main/java/org/linkki/core/ui/aspects/annotation/BindVisible.java
+++ vaadin-flow/core/src/main/java/org/linkki/core/ui/aspects/annotation/BindVisible.java
@@ -19,6 +19,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.linkki.core.binding.BindingContext;
 import org.linkki.core.binding.descriptor.aspect.LinkkiAspectDefinition;
 import org.linkki.core.binding.descriptor.aspect.annotation.AspectDefinitionCreator;
 import org.linkki.core.binding.descriptor.aspect.annotation.LinkkiAspect;
@@ -29,14 +30,30 @@ import org.linkki.core.ui.aspects.annotation.BindVisible.BindVisibleAspectDefini
 import org.linkki.core.ui.layout.annotation.UISection;
 
 /**
- * This aspect annotation sets the visibility of a component to {@link VisibleType#DYNAMIC}, determining
- * its visibility by calling the <b>is[PropertyName]Visible()</b> method.
+ * This aspect annotation sets the visibility of a component to {@link VisibleType#DYNAMIC}, determining its visibility
+ * by calling the <b>is[PropertyName]Visible()</b> method. When annotating an entire PMO the method is
+ * <b>isVisible()</b>.
  * <p>
- * If used on a {@link ContainerPmo}, the visibility of the surrounding {@link UISection} is set instead
- * of the table's visibility.
+ * Apart from an initial update upon creation, updates of child bindings are skipped if a PMO is invisible.
+ *
+ * <p>
+ * <b>Note</b>: To ensure stability during the initial update phase:
+ * <ul>
+ *     <li>
+ *         Instantiate PMOs with a temporary model object, then switch to the actual model object and invoke
+ *         {@link BindingContext#uiUpdated()}.
+ *     </li>
+ *     <li>
+ *         Implement null and exception checks in getter methods without direct model binding.
+ *     </li>
+ * </ul>
+ *
+ * <p>
+ * If used on a {@link ContainerPmo}, the visibility of the surrounding {@link UISection} is set instead of the table's
+ * visibility.
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target(value = { ElementType.FIELD, ElementType.METHOD, ElementType.TYPE })
+@Target(value = {ElementType.FIELD, ElementType.METHOD, ElementType.TYPE})
 @LinkkiAspect(BindVisibleAspectDefinitionCreator.class)
 public @interface BindVisible {
 
