602e946a370a3fd916214188e338d86452fc3c81
Luzia <luzia.dorsch@faktorzehn.de>
2024-07-16 14:40:56 +0200

    LIN-2628/LIN-3922 Adapt AddressPmo to description in tutorial
    Change-Id: I99cba2762200ae2ff7719922d141acef13e05b2c

--- vaadin-flow/samples/tutorial/src/main/java/org/linkki/samples/appsample/pmo/dialog/AddressPmo.java
+++ vaadin-flow/samples/tutorial/src/main/java/org/linkki/samples/appsample/pmo/dialog/AddressPmo.java
@@ -13,55 +13,54 @@
  */
 package org.linkki.samples.appsample.pmo.dialog;
 
+import org.linkki.core.defaults.ui.aspects.types.RequiredType;
 import org.linkki.core.pmo.ModelObject;
 import org.linkki.core.ui.element.annotation.UITextField;
 import org.linkki.core.ui.layout.annotation.UISection;
 import org.linkki.samples.appsample.model.Address;
 
-// tag::addressPmo1[]
+// tag::addressPmo[]
 @UISection
 public class AddressPmo {
 
-    // tag::modelBinding[]
+    // tag::modelBinding1[]
     private final Address address;
 
-    // end::modelBinding[]
+    // end::modelBinding1[]
     public AddressPmo(Address address) {
         this.address = address;
     }
 
-    // tag::modelBinding[]
+    // tag::modelBinding2[]
     @ModelObject
     public Address getAddress() {
         return address;
     }
 
-    // end::addressPmo1[]
-    @UITextField(position = 10, label = "Street", modelAttribute = "street")
+    @UITextField(position = 10, label = "Street", modelAttribute = "street", required = RequiredType.REQUIRED)
     public void street() {
         // model binding
     }
 
-    @UITextField(position = 20, label = "Number", modelAttribute = "streetNumber")
+    @UITextField(position = 20, label = "Number", modelAttribute = "streetNumber", required = RequiredType.REQUIRED)
     public void streetNumber() {
         // model binding
     }
 
-    @UITextField(position = 30, label = "Postal Code", modelAttribute = "postalCode")
+    @UITextField(position = 30, label = "Postal Code", modelAttribute = "postalCode", required = RequiredType.REQUIRED)
     public void postalCode() {
         // model binding
     }
 
-    @UITextField(position = 40, label = "City", modelAttribute = "city")
+    @UITextField(position = 40, label = "City", modelAttribute = "city", required = RequiredType.REQUIRED)
     public void city() {
         // model binding
     }
 
-    @UITextField(position = 50, label = "Country", modelAttribute = "country")
+    @UITextField(position = 50, label = "Country", modelAttribute = "country", required = RequiredType.REQUIRED)
     public void country() {
         // model binding
     }
-    // end::modelBinding[]
-    // tag::addressPmo2[]
+    // end::modelBinding2[]
 }
-// end::addressPmo2[]
+// end::addressPmo[]
